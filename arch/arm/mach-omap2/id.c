e7592f4efc2a79c302bb143858a1b66b202e517e
artas182x <artas182x@gmail.com>
2013-09-06 19:52:47 +0200

    Prepare for OC

--- arch/arm/mach-omap2/id.c
+++ arch/arm/mach-omap2/id.c
@@ -251,6 +251,8 @@ static void __init omap4_check_features(void)
 		/* Standard device */
 		if (cpu_is_omap443x())
 			omap4_features |= OMAP4_HAS_MPU_1GHZ;
+			omap4_features |= OMAP4_HAS_MPU_1_2GHZ;
+			omap4_features |= OMAP4_HAS_IVA_430MHZ;
 		else if (cpu_is_omap446x()) {
 			omap4_features |= OMAP4_HAS_MPU_1_2GHZ;
 			omap4_features |= OMAP4_HAS_IVA_430MHZ;
